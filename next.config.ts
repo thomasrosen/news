629035a6e2dff5c360687578642f06f2aa5ab965
thomasrosen <thomas.rosen@me.com>
2026-01-13 22:23:53 +0100

    trying to fix build on uberspace

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ let nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  images: {
+    unoptimized: true,
+  },
 }
 
 const withMDX = createMDX({
