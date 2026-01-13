55001a137cb1d739f2cf3a175f7f234a97ce6253
thomasrosen <thomas.rosen@me.com>
2026-01-13 23:24:20 +0100

    Update remarkPrefixImages.ts

--- src/lib/remarkPrefixImages.ts
+++ src/lib/remarkPrefixImages.ts
@@ -1,13 +1,12 @@
 import type { Root } from 'mdast'
 import type { Plugin } from 'unified'
 import { visit } from 'unist-util-visit'
-import { getRootPath } from './getRootPath'
 
 export const remarkPrefixImages: Plugin<[], Root> = () => {
   return (tree: Root) => {
     visit(tree, 'image', (node) => {
       if (node.url) {
-        node.url = `${getRootPath()}/content/media/${node.url}`
+        node.url = `@@/content/media/${node.url}`
         console.log('node.url', node.url)
         return false;
       }
