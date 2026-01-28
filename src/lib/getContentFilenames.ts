acf401439b79d9b4d25950f24e83c129c19a41d6
thomasrosen <thomas.rosen@me.com>
2026-01-28 16:18:51 +0100

    trying with content inside public

--- src/lib/getContentFilenames.ts
+++ src/lib/getContentFilenames.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export async function getContentFilenames({ subpath = 'articles' }: { subpath: string }): Promise<string[]> {
-  const folderPath = path.join(process.cwd(), 'content', subpath);
+  const folderPath = path.join(process.cwd(), 'public', 'content', subpath);
   const files = await fs.readdir(folderPath)
   const extension = '.mdx' // Markdown files
   return files
